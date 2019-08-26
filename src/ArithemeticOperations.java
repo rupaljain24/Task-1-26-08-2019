65a60b244637edb263f93fd1d854e35963c24e0d
rupal24jain <rupal24jain@rupal24>
2019-08-26 20:13:51 +0530

    My first commit

---
+++ src/ArithemeticOperations.java
@@ -0,0 +1,12 @@
+import com.operations.*;
+public class ArithemeticOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Addition a1=new Addition();
+        a1.add(20,10);
+        Subraction s1=new Subraction();
+        s1.sub(50,10);
+	}
+
+}
